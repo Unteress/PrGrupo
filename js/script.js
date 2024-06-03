f0f6d851ce2e81b845b6b57916d239b7d84ddb65
Unteress <cjzb20011210@gmail.com>
2024-06-03 11:59:18 -0500

    BuscarProductov2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function buscarProducto() {
     const busqueda = document.getElementById('buscarProducto').value.toLowerCase();
     const filas = document.getElementById('tablaProductos').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    for (let i = 0; i < filas.length; i++) {
+        const producto = filas[i].getElementsByTagName('td')[1].textContent.toLowerCase();
+        if (producto.includes(busqueda)) {
+            filas[i].style.display = '';
+        } else {
+            filas[i].style.display = 'none';
+        }
+    }
+}
